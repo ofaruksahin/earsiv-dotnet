13cbcef7b5b1408972f17ea00fb1d5052c9c256f
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2023-06-06 23:28:10 +0300

    Fatura imzalamak için sms gönderme işlemleri

---
+++ GIBDotNet/Commands/RequestModels/SendVerifySMSCommandRequestModel.cs
@@ -0,0 +1,18 @@
+﻿namespace GIBDotNet.Commands.RequestModels
+{
+    public class SendVerifySMSCommandRequestModel
+    {
+        public string UserId { get; private set; }
+        public string Token { get; private set; }
+
+        public SendVerifySMSCommandRequestModel()
+        {
+        }
+
+        public SendVerifySMSCommandRequestModel(string userId,string token)
+        {
+            UserId = userId;
+            Token = token;
+        }
+    }
+}
---
+++ GIBDotNet/Commands/SendVerifySMSCommand.cs
@@ -0,0 +1,27 @@
+﻿using GIBDotNet.Commands.RequestModels;
+using GIBDotNet.Commands.ResponseModels;
+using GIBDotNet.Contracts;
+using System.Threading.Tasks;
+
+namespace GIBDotNet.Commands
+{
+    public class SendVerifySMSCommand : BaseGIBCommand, IGIBCommand<SendVerifySMSCommandRequestModel, SendVerifySMSCommandResponseModel>
+    {
+        public async Task<BaseGIBResponse<SendVerifySMSCommandResponseModel>> DispatchCommand(SendVerifySMSCommandRequestModel requestModel)
+        {
+            return default;
+        }
+
+        public class SendVerifySMSCommandStep1 : BaseGIBCommand
+        {
+        }
+
+        public class SendVerifySMSCommandStep2 : BaseGIBCommand
+        {
+        }
+
+        public class SendVerifySMSCommandStep3 : BaseGIBCommand
+        {
+        }
+    }
+}
